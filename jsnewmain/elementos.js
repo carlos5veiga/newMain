4393240fce250a9a23c2d0e55cae8a1f715f6c86
carlos5veiga <carlos5veiga@gmail.com>
2024-01-03 08:52:44 -0300

    array de dados de vídeos

--- jsnewmain/elementos.js
+++ jsnewmain/elementos.js
@@ -82,6 +82,28 @@ if (rodape) {
     rodape.innerHTML = conteudoRodape;
 }
 
+
+const dados = [
+    {
+        "tituloVideo":"Harmonia 01 - Tríades",
+        "tituloCabecalho":"O que é tríade?",
+        "codigoVideo":"GadkL8Pd5cg",
+        "resumoVideo":"Conheça o elemento básico da harmonia."
+    },
+    {
+        "tituloVideo":"Harmonia 02 - Maior, menor e diminuto",
+        "tituloCabecalho":"Maior, Menor ou Diminuto?",
+        "codigoVideo":"6CtFw23x5qo",
+        "resumoVideo":"Aprenda a classificar intervalos e tríades."
+    },
+    {
+        "tituloVideo":"Harmonia 03 - Acordes Maiores e menores",
+        "tituloCabecalho":"Maior ou Menor?",
+        "codigoVideo":"UAelunGDzgM",
+        "resumoVideo":"Reconheça pelo som se o acorde é Maior ou Menor."
+    }
+]
+
 for (let index = 0; index < 3; index++) {
     let newSection = document.createElement('section');
     newSection.setAttribute('class','carrossel');
@@ -93,7 +115,7 @@ for (let index = 0; index < 3; index++) {
 
     let newH2 = document.createElement('h2');
     newH2.setAttribute('class','carrossel__titulo');
-    newH2.textContent = 'Titulo';
+    newH2.textContent = dados[index].tituloCabecalho;
     newSection.appendChild(newH2);
 
     let newDiv1 = document.createElement('div');
@@ -106,21 +128,25 @@ for (let index = 0; index < 3; index++) {
     newDiv2.setAttribute('id', 'div2');
     newDiv1.appendChild(newDiv2);
 
+    const linkVideo = 'https://youtu.be/' + dados[index].codigoVideo;
+
     let newA = document.createElement('a');
     newA.setAttribute('target','_blank');
-    newA.setAttribute('href','https://youtu.be/lDK61UKmiQA');
+    newA.setAttribute('href', linkVideo);
     newDiv2.appendChild(newA);
 
+    const imagemVideo = 'https://img.youtube.com/vi/' + dados[index].codigoVideo + '/hqdefault.jpg';
+
     let newImg = document.createElement('img');
-    newImg.setAttribute('src','https://img.youtube.com/vi/lDK61UKmiQA/hqdefault.jpg');
-    newImg.setAttribute('alt','Imagem do vídeo Solfejo 03 Software');
+    newImg.setAttribute('src', imagemVideo);
+    newImg.setAttribute('alt','Imagem do vídeo');
     newImg.setAttribute('class','descricao__imagem');
     newImg.setAttribute('height','160px');
     newA.appendChild(newImg);
 
     let newP = document.createElement('p');
     newP.setAttribute('class','texto-card__imagem-texto');
-    newP.textContent = 'Conteúdo do card';
+    newP.textContent = dados[index].resumoVideo;
     newDiv2.appendChild(newP);
 
     let newDiv3 = document.createElement('div');
@@ -129,7 +155,7 @@ for (let index = 0; index < 3; index++) {
 
     let newA2 = document.createElement('a');
     newA2.setAttribute('target','_blank');
-    newA2.setAttribute('href','https://youtu.be/lDK61UKmiQA');
+    newA2.setAttribute('href', linkVideo);
     newA2.setAttribute('class','botao-conhecer');
     newA2.textContent = 'Assista';
     newDiv3.appendChild(newA2);
