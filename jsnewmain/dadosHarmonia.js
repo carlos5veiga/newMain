1692917281a98effa1d55f7292a8ef454430ffcc
carlos5veiga <carlos5veiga@gmail.com>
2024-01-14 07:50:06 -0300

    otimo resultado

---
+++ jsnewmain/dadosHarmonia.js
@@ -0,0 +1,124 @@
+const dados = [
+    {
+      tituloVideo: 'Solfejo 7 a cappella',
+      tituloCabecalho: 'Solfejo 7 a cappella',
+      codigoVideo: 'Z22n5InVfLM',
+      resumoVideo: 'Pratique este lindo solfejo.'
+    },
+    {
+      tituloVideo: 'Solfejo 6 a cappella',
+      tituloCabecalho: '',
+      codigoVideo: 'eTdNmNzyyyk',
+      resumoVideo: ''
+    },
+    {
+      tituloVideo: 'Solfejo 5 a cappella',
+      tituloCabecalho: '',
+      codigoVideo: 'RnEPTDGnQGY',
+      resumoVideo: ''
+    },
+    {
+      tituloVideo: 'Solfejo 4 a cappella',
+      tituloCabecalho: '',
+      codigoVideo: 'BLe3kx9QK38',
+      resumoVideo: ''
+    },
+    {
+      tituloVideo: 'Solfejo 3 a cappella',
+      tituloCabecalho: '',
+      codigoVideo: '89vkvO5tp0w',
+      resumoVideo: ''
+    },
+    {
+      tituloVideo: 'Solfejo Forrest Gump',
+      tituloCabecalho: '',
+      codigoVideo: 'B6-hI75z2TU',
+      resumoVideo: ''
+    },
+    {
+      tituloVideo: 'Sozinho',
+      tituloCabecalho: '',
+      codigoVideo: '5g1OfE2HRRg',
+      resumoVideo: ''
+    },
+    {
+      tituloVideo: 'Solfejo No woman, no cry #bobmarley #reggae #solfejo',
+      tituloCabecalho: '',
+      codigoVideo: 'rZRtB2ec-bY',
+      resumoVideo: ''
+    },
+    {
+      tituloVideo: 'Solfejo Ovelha Negra #ritalee #ovelhanegra #rock',
+      tituloCabecalho: '',
+      codigoVideo: 's5859NAdIJg',
+      resumoVideo: ''
+    },
+    {
+      tituloVideo: 'Solfejo Heal the world',
+      tituloCabecalho: '',
+      codigoVideo: 'OF5InwfdeDg',
+      resumoVideo: ''
+    },
+    {
+      tituloVideo: 'Solfejo de Azul da cor do mar',
+      tituloCabecalho: '',
+      codigoVideo: 'Je-3I1IfkW0',
+      resumoVideo: ''
+    },
+    {
+      tituloVideo: 'Livro top',
+      tituloCabecalho: '',
+      codigoVideo: '6VXmDSu38eE',
+      resumoVideo: ''
+    },
+    {
+      tituloVideo: 'Solfejo 2 a cappella',
+      tituloCabecalho: '',
+      codigoVideo: 'NVDKzvZHRkE',
+      resumoVideo: ''
+    },
+    {
+      tituloVideo: 'Solfejo 01 a cappella',
+      tituloCabecalho: '',
+      codigoVideo: 'CFl-FALbeMg',
+      resumoVideo: ''
+    },
+    {
+      tituloVideo: 'Solfejo 36',
+      tituloCabecalho: '',
+      codigoVideo: 'GNV-Ri-w9lc',
+      resumoVideo: 'https://carlosveigafilho.com.br/solfejos/SolfejosParaPraticar.pdf.'
+    },
+    {
+      tituloVideo: 'Grupo Vocal Pentágono no Hotel Fiesta - 18/10/23 - Evento do Cremeb - Dia dos médicos',
+      tituloCabecalho: '',
+      codigoVideo: 'vfaq0RHdbNE',
+      resumoVideo: ''
+    },
+    {
+      tituloVideo: 'Exercitando os graus',
+      tituloCabecalho: '',
+      codigoVideo: 'F6eC05rbOnc',
+      resumoVideo: ''
+    },
+    {
+      tituloVideo: 'Leitura rítmica 01',
+      tituloCabecalho: '',
+      codigoVideo: 'esLM7nyAdE8',
+      resumoVideo: ''
+    },
+    {
+      tituloVideo: 'Modo Mixolídio - Hey Jude',
+      tituloCabecalho: '',
+      codigoVideo: 'eR6E8LeHeqY',
+      resumoVideo: ''
+    },
+    {
+      tituloVideo: 'Praticando os graus em todos os lugares',
+      tituloCabecalho: '',
+      codigoVideo: '52GNwPQAe00',
+      resumoVideo: ''
+    }
+  ]
+
+  export { dados };
