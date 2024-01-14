035d4aac4ed29fe54aeca95971e39deda219714b
carlos5veiga <carlos5veiga@gmail.com>
2024-01-14 11:56:02 -0300

    ampliando os solfejos

---
+++ jsnewmain/dadosPlaylistSolfejo.js
@@ -0,0 +1,436 @@
+const dados = [
+  {
+    tituloVideo: 'Solfejo01 - Introdução',
+    tituloCabecalho: 'O que é o Solfejo?',
+    codigoVideo: 'qqw3War57BQ',
+    resumoVideo: 'Meu primeiro vídeo, gravado em 2010. O início de todo o trabalho do canal Tutoriais em Música.'
+  },
+  {
+    tituloVideo: 'Solfejo02 - Tônica',
+    tituloCabecalho: 'O que é a tônica',
+    codigoVideo: '6v4KHwGHXEQ',
+    resumoVideo: 'Perceba auditivamente e entenda o conceito.'
+  },
+  {
+    tituloVideo: 'Solfejo 03 - Software',
+    tituloCabecalho: 'App para os graus',
+    codigoVideo: 'lDK61UKmiQA',
+    resumoVideo: 'Acesse um aplicativo feito por mim para auxiliar o seu aprendizado.'
+  },
+  {
+    tituloVideo: 'Solfejo 04 - Pentagrama',
+    tituloCabecalho: 'A notação musical',
+    codigoVideo: 'QacA2f6L_1U',
+    resumoVideo: 'Aprenda como as notas musicais são representadas em um pentagrama.'
+  },
+  {
+    tituloVideo: 'Solfejo 05 - Estrutura da Escala Maior',
+    tituloCabecalho: 'Estrutura da escala maior',
+    codigoVideo: 'nL5ecTTK4KE',
+    resumoVideo: 'Tópico essencial para desenvolver o solfejo.'
+  },
+  {
+    tituloVideo: 'Solfejo 06 - Exercícios',
+    tituloCabecalho: 'Exercícios',
+    codigoVideo: 'dRhY885r-LM',
+    resumoVideo: 'Um pouco de exercício para complementar os estudos.'
+  },
+  {
+    tituloVideo: 'Solfejo 07 - Sol Maior',
+    tituloCabecalho: 'Sol Maior',
+    codigoVideo: '9iPbKvB5x1Q',
+    resumoVideo: 'Conheça a escala de Sol Maior'
+  },
+  {
+    tituloVideo: 'Solfejo 08 - Sustenido',
+    tituloCabecalho: 'Sustenido',
+    codigoVideo: 'CvkvfuNgpSU',
+    resumoVideo: 'Comece a descobrir as alterações das notas.'
+  },
+  {
+    tituloVideo: 'Solfejo 09 - Armaduras',
+    tituloCabecalho: 'Armaduras',
+    codigoVideo: 'NCwZHwEeFNI',
+    resumoVideo: 'Simplificando a partitura com a armadura.'
+  },
+  {
+    tituloVideo: 'Solfejo 10 - Claves',
+    tituloCabecalho: 'Claves',
+    codigoVideo: 'Juix6j-RXE4',
+    resumoVideo: 'Entenda porque existem as claves.'
+  },
+  {
+    tituloVideo: 'Solfejo 11 - Clave de sol',
+    tituloCabecalho: 'Clave de sol',
+    codigoVideo: 'EBP3KxQ9sI0',
+    resumoVideo: 'Conheça este símbolo mundialmente famoso.'
+  },
+  {
+    tituloVideo: 'Solfejo 12 - Sol Maior 2',
+    tituloCabecalho: 'Sol Maior 2',
+    codigoVideo: 'pfhAcB888VI',
+    resumoVideo: 'Aprofundando na tonalidade de Sol Maior.'
+  },
+  {
+    tituloVideo: 'Solfejo 13 - Lá Maior',
+    tituloCabecalho: 'Lá maior',
+    codigoVideo: 'CwvUI_epm4o',
+    resumoVideo: 'Conheça a estrutura da escala de Lá Maior.'
+  },
+  {
+    tituloVideo: 'Solfejo 14 - Ré Maior',
+    tituloCabecalho: 'Ré maior',
+    codigoVideo: 'y6Fdmqjppl8',
+    resumoVideo: 'Conheça a estrutura da escala de Ré Maior.'
+  },
+  {
+    tituloVideo: 'Solfejo 15 - Primeiro Solfejo',
+    tituloCabecalho: '',
+    codigoVideo: 'EAOA3NUS0qc',
+    resumoVideo: ''
+  },
+  {
+    tituloVideo: 'Solfejo 16 - Segundo Solfejo',
+    tituloCabecalho: '',
+    codigoVideo: 'My8Jlt57LeY',
+    resumoVideo: ''
+  },
+  {
+    tituloVideo: 'Solfejo 17 - Partes de coral',
+    tituloCabecalho: '',
+    codigoVideo: 'kM6skY2bJRU',
+    resumoVideo: ''
+  },
+  {
+    tituloVideo: 'Solfejo 18 - Elementos Ritmicos',
+    tituloCabecalho: '',
+    codigoVideo: 'NdfD_V0dk4Y',
+    resumoVideo: ''
+  },
+  {
+    tituloVideo: 'Solfejo 19 - Símbolos rítmicos',
+    tituloCabecalho: '',
+    codigoVideo: 'A28lh01IcZw',
+    resumoVideo: ''
+  },
+  {
+    tituloVideo: 'Solfejo 20 - Compassos',
+    tituloCabecalho: '',
+    codigoVideo: 'VUGYRGlk1AA',
+    resumoVideo: ''
+  },
+  {
+    tituloVideo: 'Solfejo 21 - Compassos Binários',
+    tituloCabecalho: '',
+    codigoVideo: 'Oqw3fNlqLDw',
+    resumoVideo: ''
+  },
+  {
+    tituloVideo: 'Solfejo 22 - Exemplos binários',
+    tituloCabecalho: '',
+    codigoVideo: 'B27vXvlVdAA',
+    resumoVideo: ''
+  },
+  {
+    tituloVideo: 'Solfejo 23 - Compassos Ternários',
+    tituloCabecalho: '',
+    codigoVideo: 'zQAj55o7qAU',
+    resumoVideo: ''
+  },
+  {
+    tituloVideo: 'Solfejo 24 - Exemplos ternários',
+    tituloCabecalho: '',
+    codigoVideo: 'mVxQ83tcm1w',
+    resumoVideo: ''
+  },
+  {
+    tituloVideo: 'Solfejo 25 - Compassos quaternários',
+    tituloCabecalho: '',
+    codigoVideo: 'lC2s9SyGs1A',
+    resumoVideo: ''
+  },
+  {
+    tituloVideo: 'Solfejo 26 - Exemplos quaternários',
+    tituloCabecalho: '',
+    codigoVideo: 'FAhpkTiYIVM',
+    resumoVideo: ''
+  },
+  {
+    tituloVideo: 'Solfejo 27 - Dobro x Metade',
+    tituloCabecalho: '',
+    codigoVideo: 'eORjOJvL4co',
+    resumoVideo: ''
+  },
+  {
+    tituloVideo: 'Solfejo 28 - Armaduras com sustenidos',
+    tituloCabecalho: '',
+    codigoVideo: 'GHzbPJ2euxU',
+    resumoVideo: ''
+  },
+  {
+    tituloVideo: 'Solfejo 29 - Armaduras com bemóis',
+    tituloCabecalho: '',
+    codigoVideo: 'zSpnqICRvKI',
+    resumoVideo: ''
+  },
+  {
+    tituloVideo: 'Solfejo 30 - Dois exemplos',
+    tituloCabecalho: '',
+    codigoVideo: '9WxJFdF9cws',
+    resumoVideo: ''
+  },
+  {
+    tituloVideo: 'Solfejo 31 - Intervalos',
+    tituloCabecalho: '',
+    codigoVideo: 'pH80v1OzVFA',
+    resumoVideo: ''
+  },
+  {
+    tituloVideo: 'Solfejo 32 - Segundas Maiores e menores',
+    tituloCabecalho: '',
+    codigoVideo: '9d-a0jI-IzY',
+    resumoVideo: ''
+  },
+  {
+    tituloVideo: 'Solfejo 33 - Terças maiores e menores',
+    tituloCabecalho: '',
+    codigoVideo: 'lEgotD1sKHk',
+    resumoVideo: ''
+  },
+  {
+    tituloVideo: 'Solfejo 34 - Oitava justa',
+    tituloCabecalho: '',
+    codigoVideo: 'RQv4yL61rLE',
+    resumoVideo: ''
+  },
+  {
+    tituloVideo: 'Solfejo 35 - Intervalos Justos x Maiores e menores',
+    tituloCabecalho: '',
+    codigoVideo: 'm4vOmT3qufY',
+    resumoVideo: ''
+  },
+  {
+    tituloVideo: 'Solfejo 36 - Sextas Maiores e menores',
+    tituloCabecalho: '',
+    codigoVideo: '5yIGHMmLYhc',
+    resumoVideo: ''
+  },
+  {
+    tituloVideo: 'Solfejo 37 - Sétimas Maiores e menores',
+    tituloCabecalho: '',
+    codigoVideo: 'mAh_Dt_AZmw',
+    resumoVideo: ''
+  },
+  {
+    tituloVideo: 'Solfejo 38 - Inversão de intervalos',
+    tituloCabecalho: '',
+    codigoVideo: 'd1mtPn229Fo',
+    resumoVideo: ''
+  },
+  {
+    tituloVideo: 'Solfejo 39 - Subdivisão binária',
+    tituloCabecalho: '',
+    codigoVideo: 'Lk0LWJV6HYE',
+    resumoVideo: ''
+  },
+  {
+    tituloVideo: 'Solfejo 40 - Clave de fá',
+    tituloCabecalho: '',
+    codigoVideo: 'gDjRHvmuz60',
+    resumoVideo: ''
+  },
+  {
+    tituloVideo: 'Solfejo 41 - Exemplos clave de fá',
+    tituloCabecalho: '',
+    codigoVideo: 'hmwXJpFgbyQ',
+    resumoVideo: ''
+  },
+  {
+    tituloVideo: 'Solfejo 42 - Escala menor primitiva',
+    tituloCabecalho: '',
+    codigoVideo: 'Ich5bNj5eEw',
+    resumoVideo: ''
+  },
+  {
+    tituloVideo: 'Solfejo 43 - Treinando leitura e escrita de música',
+    tituloCabecalho: '',
+    codigoVideo: 'n_WRtA29xZE',
+    resumoVideo: ''
+  },
+  {
+    tituloVideo: 'Solfejo 44 - Ditado melódico',
+    tituloCabecalho: '',
+    codigoVideo: 'Svz5AmMOtCY',
+    resumoVideo: ''
+  },
+  {
+    tituloVideo: 'Solfejo 45 - Escala menor harmônica',
+    tituloCabecalho: '',
+    codigoVideo: 'yMuWqTfWi9I',
+    resumoVideo: ''
+  },
+  {
+    tituloVideo: 'Solfejo 46 - Escala menor melódica',
+    tituloCabecalho: '',
+    codigoVideo: 'd3A9jLeDzgw',
+    resumoVideo: ''
+  },
+  {
+    tituloVideo: 'Solfejo 47 - Escala Bachiana e resumo',
+    tituloCabecalho: '',
+    codigoVideo: 'fMD-Q8qal7w',
+    resumoVideo: ''
+  },
+  {
+    tituloVideo: 'Solfejo 48 - Grupos rítmicos',
+    tituloCabecalho: '',
+    codigoVideo: 'u59RMgkflv8',
+    resumoVideo: ''
+  },
+  {
+    tituloVideo: 'Solfejo 49 - Tercinas ou quiálteras de 3',
+    tituloCabecalho: '',
+    codigoVideo: '07VBGhIT7o0',
+    resumoVideo: ''
+  },
+  {
+    tituloVideo: 'Solfejo 50 - Compassos compostos',
+    tituloCabecalho: '',
+    codigoVideo: 'DtaOA3sJOd0',
+    resumoVideo: ''
+  },
+  {
+    tituloVideo: 'Solfejo 51 - Ligaduras de duração',
+    tituloCabecalho: '',
+    codigoVideo: 'N8Z7Up1Ye0I',
+    resumoVideo: ''
+  },
+  {
+    tituloVideo: 'Solfejo 52 - Clave de dó',
+    tituloCabecalho: '',
+    codigoVideo: 'O3Xvvm1wE50',
+    resumoVideo: ''
+  },
+  {
+    tituloVideo: 'Solfejo 53 - Relação entre as claves',
+    tituloCabecalho: '',
+    codigoVideo: 'wR977l8JVcM',
+    resumoVideo: ''
+  },
+  {
+    tituloVideo: 'Solfejo 54 - Ligaduras e ponto de aumento',
+    tituloCabecalho: '',
+    codigoVideo: 'JRwZ7yglHDA',
+    resumoVideo: ''
+  },
+  {
+    tituloVideo: 'Solfejo 55 - Síncopes',
+    tituloCabecalho: '',
+    codigoVideo: 'Ofu2-5nyzN0',
+    resumoVideo: ''
+  },
+  {
+    tituloVideo: 'Anacruse, Tético e Acéfalo',
+    tituloCabecalho: '',
+    codigoVideo: 'aGX17QJauoA',
+    resumoVideo: ''
+  },
+  {
+    tituloVideo: 'Solfejo 57 - Modo jônio',
+    tituloCabecalho: '',
+    codigoVideo: 'VeboQODP20I',
+    resumoVideo: ''
+  },
+  {
+    tituloVideo: 'Solfejo 58 - Modo dórico',
+    tituloCabecalho: '',
+    codigoVideo: 'EgxyJAESLnY',
+    resumoVideo: ''
+  },
+  {
+    tituloVideo: 'Solfejo 59 - Modo frígio',
+    tituloCabecalho: '',
+    codigoVideo: '8XaAaph-dfg',
+    resumoVideo: ''
+  },
+  {
+    tituloVideo: 'Solfejo 60 - Modo lídio',
+    tituloCabecalho: '',
+    codigoVideo: 'V2j4Fv_ql28',
+    resumoVideo: ''
+  },
+  {
+    tituloVideo: 'Solfejo 61 - Modo mixolídio',
+    tituloCabecalho: '',
+    codigoVideo: 'J09UpN_Ti7M',
+    resumoVideo: ''
+  },
+  {
+    tituloVideo: 'Solfejo 62 - Modo eólio',
+    tituloCabecalho: '',
+    codigoVideo: 'pDDoh-uqUgQ',
+    resumoVideo: ''
+  },
+  {
+    tituloVideo: 'Solfejo 63 - Modo lócrio',
+    tituloCabecalho: '',
+    codigoVideo: 'RhvCulmPm08',
+    resumoVideo: ''
+  },
+  {
+    tituloVideo: 'Seqüências numéricas - Tutoriais em música',
+    tituloCabecalho: '',
+    codigoVideo: '9L4fI4pCO0Q',
+    resumoVideo: ''
+  },
+  {
+    tituloVideo: 'Tutoriais em música - App graus da escala maior',
+    tituloCabecalho: '',
+    codigoVideo: 'jJizNXItjrE',
+    resumoVideo: ''
+  },
+  {
+    tituloVideo: 'Tutoriais em música - Software graus da escala maior',
+    tituloCabecalho: '',
+    codigoVideo: 'S7CWMdhHKV4',
+    resumoVideo: ''
+  },
+  {
+    tituloVideo: 'Tutoriais em música - Sequencias Numéricas 2',
+    tituloCabecalho: '',
+    codigoVideo: 'oo9SdH8y1cw',
+    resumoVideo: ''
+  },
+  {
+    tituloVideo: 'Solfejo 68 - Ditados Sequencias Numéricas',
+    tituloCabecalho: '',
+    codigoVideo: 'k0uJvbMT9JY',
+    resumoVideo: ''
+  },
+  {
+    tituloVideo: 'Tutoriais em música - Sequências na pauta',
+    tituloCabecalho: '',
+    codigoVideo: 'fLHa8M3W7rk',
+    resumoVideo: ''
+  },
+  {
+    tituloVideo: 'Tutoriais em música - Sequencias Numéricas 3',
+    tituloCabecalho: '',
+    codigoVideo: 'vD3d1qNHcBM',
+    resumoVideo: ''
+  },
+  {
+    tituloVideo: 'Tutoriais em música - Sequencias Numéricas 4',
+    tituloCabecalho: '',
+    codigoVideo: 'nNGRfsB1d1k',
+    resumoVideo: ''
+  },
+  {
+    tituloVideo: 'Solfejo 72 - Melodia simples',
+    tituloCabecalho: '',
+    codigoVideo: 'rDwm273u614',
+    resumoVideo: ''
+  }
+]
+
+export { dados };
---
+++ jsnewmain/elementosSolfejo.js
@@ -0,0 +1,204 @@
+import { dados } from './dadosPlaylistSolfejo.js'
+
+const cabecalho = document.getElementById("cabecalho");
+if (cabecalho) {
+    cabecalho.innerHTML = `
+    <input type="checkbox" id="menu" class="cabecalho__botao">
+    <label for="menu" class="rotulo">
+        <span class="cabecalho__menu-hamburger"></span>
+    </label>
+    <ul class="lista-menu">
+        <li class="lista-menu__titulo">Categorias</li>
+        <li class="lista-menu__item"><a href="../tutoriais.html" class="lista-menu__link">Tutoriais</a></li>
+        <li class="lista-menu__item"><a target="_blank" href="https://tutoriaisemmusica.com.br" class="lista-menu__link">Cursos</a></li>
+        <li class="lista-menu__item"><a href="../ferramentas.html" class="lista-menu__link">Ferramentas</a></li>
+        <li class="lista-menu__item"><a href="../livros.html" class="lista-menu__link">Livros</a></li>
+        <li class="lista-menu__item"><a target="_blank" href="https://carlosveigafilho.com.br/repertorio/index.html" class="lista-menu__link">Corais</a></li>
+    </ul>
+    <a href="../index.html"><img src="https://carlosveigafilho.com.br/img/LogoTuts40.png" class="cabecalho__imagem" alt="Logo dos Tutoriais em Música"></a>
+    <h1 class="titulo-principal">Carlos Veiga Filho</h1>
+
+    <ul class="opcoes">
+        <li class="opcoes__item"><a href="../tutoriais.html" class="opcoes__link">Tutoriais</a></li>
+        <li class="opcoes__item"><a target="_blank" href="https://tutoriaisemmusica.com.br" class="opcoes__link">Cursos</a></li>
+        <li class="opcoes__item"><a href="../ferramentas.html" class="opcoes__link">Ferramentas</a></li>
+        <li class="opcoes__item"><a href="../livros.html" class="opcoes__link">Livros</a></li>
+        <li class="opcoes__item"><a target="_blank" href="https://carlosveigafilho.com.br/repertorio/index.html" class="opcoes__link">Corais</a></li>
+    </ul>
+`;
+}
+
+const cursos = document.getElementById("secao-cursos");
+if (cursos) {
+    cursos.innerHTML = `
+    <h2 class="topicos__titulo">Cursos</h2>
+    <ul class="topicos__lista">
+        <li class="topicos__item">
+            <a class="topicos__link" href="https://tutoriaisemmusica.com.br/scoreplay" target="_blank">Scoreplay</a>
+        </li>
+        <li class="topicos__item">
+            <a class="topicos__link" href="https://tutoriaisemmusica.com.br/15melodiasparatocar" target="_blank">15 Melodias</a>
+        </li>
+        <li class="topicos__item">
+            <a class="topicos__link" href="https://tutoriaisemmusica.com.br/teoriamusical" target="_blank">Teoria</a>
+        </li>
+        <li class="topicos__item">
+            <a class="topicos__link" href="https://tutoriaisemmusica.com.br/intervalosmusicais" target="_blank">Intervalos</a>
+        </li>
+        <li class="topicos__item">
+            <a class="topicos__link" href="https://tutoriaisemmusica.com.br/finaledescomplicado" target="_blank">Finale</a>
+        </li>
+    </ul>
+`;
+}
+
+
+const playlists = document.getElementById("secao-playlists");
+if (playlists) {
+    playlists.innerHTML = `
+    <h2 class="topicos__titulo">Playlists do youtube</h2>
+    <ul class="topicos__lista">
+        <li class="topicos__item">
+            <a class="topicos__link" href="https://www.youtube.com/playlist?list=PLPPAfvKjYPk2k7d1KcoGJxPVotCPQuCYb" target="_blank">Minicurso Solfejo</a>
+        </li>
+        <li class="topicos__item">
+            <a class="topicos__link" href="https://www.youtube.com/playlist?list=PLPPAfvKjYPk0sxktVnfbIcs2LPiFBTQFw" target="_blank">Estudando Música</a>
+        </li>
+        <li class="topicos__item">
+            <a class="topicos__link" href="https://www.youtube.com/playlist?list=PLPPAfvKjYPk2XlppJArrQSWMf2cenDLxi" target="_blank">Música e Metrônomo</a>
+        </li>
+        <li class="topicos__item">
+            <a class="topicos__link" href="https://www.youtube.com/playlist?list=PLPPAfvKjYPk0s3tSiLsMydzLy-lrBqzIh" target="_blank">Harmonia na Prática</a>
+        </li>
+    </ul>
+`;
+}
+
+const livroAnHarm = document.getElementById("div-card-livroAnHarm");
+if (livroAnHarm) {
+    livroAnHarm.innerHTML = `
+        <div class="card__descricao">
+            <div class="descricao">
+                <h2 class="descricao__titulo">Análise Harmônica para Estudantes de Música</h2>
+                <h3 class="descricao__categoria">Livro</h3>
+                <p class="descricao__texto">Aprenda a fazer uma análise harmônica completa.</p>
+            </div>
+            <a target="_blank" href="https://www.amazon.com.br/An%C3%A1lise-Harm%C3%B4nica-para-Estudantes-M%C3%BAsica-ebook/dp/B09CCMDD6Q/ref=sr_1_1?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=2N6LPKLZMKXNO&keywords=an%C3%A1lise+harm%C3%B4nica+para+estudantes+de+m%C3%BAsica&qid=1692020657&sprefix=an%C3%A1lise+harm%C3%B4nica+para+estudantes+de+m%C3%BAsica%2Caps%2C201&sr=8-1">
+                <img src="./img/LivroAnHarm.jpg" alt="Capa do livro Análise Harmônica para Estudantes de Música" class="descricao__imagem">
+            </a>
+        </div>
+        <div class="card__botoes">
+            <a target="_blank" href="https://carlosveigafilho.com.br/divulgacao/livros/DivulgLivro.html" class="botao-conhecer">Conheça</a>
+            <a target="_blank" href="https://www.amazon.com.br/An%C3%A1lise-Harm%C3%B4nica-para-Estudantes-M%C3%BAsica-ebook/dp/B09CCMDD6Q/ref=sr_1_1?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=2N6LPKLZMKXNO&keywords=an%C3%A1lise+harm%C3%B4nica+para+estudantes+de+m%C3%BAsica&qid=1692020657&sprefix=an%C3%A1lise+harm%C3%B4nica+para+estudantes+de+m%C3%BAsica%2Caps%2C201&sr=8-1" class="botao-comprar">Compre</a>
+        </div>
+`;
+}
+
+
+const divCardScoreplay = document.getElementById("div-card-scoreplay");
+if (divCardScoreplay) {
+    divCardScoreplay.innerHTML = `
+        <div class="card__imagem-solo">
+            <a target="_blank" href="https://tutoriaisemmusica.com.br/scoreplay">
+                <img src="../../img/Scoreplay.jpg" alt="Imagem do curso scoreplay" class="descricao__imagem" height="160px">
+            </a>
+        </div>
+        <div class="card__botao-solo">
+            <a target="_blank" href="https://tutoriaisemmusica.com.br/scoreplay" class="botao-conhecer">Conheça</a>
+        </div>
+`;
+}
+
+const divCardTeoria = document.getElementById("div-card-teoria");
+if (divCardTeoria) {
+    divCardTeoria.innerHTML = `
+        <div class="card__imagem-solo">
+            <a target="_blank" href="https://tutoriaisemmusica.com.br/teoriamusical">
+                <img src="../../img/Teoria Musical.jpg" alt="Imagem do curso teoria musical" class="descricao__imagem" height="160px">
+            </a>
+        </div>
+        <div class="card__botao-solo">
+            <a target="_blank" href="https://tutoriaisemmusica.com.br/teoriamusical" class="botao-conhecer">Conheça</a>
+        </div>
+`;
+}
+
+const currentYear = new Date().getFullYear();
+
+const conteudoRodape = '<h2 class="rodape__titulo">Carlos Veiga Filho</h2><h3 class="rodape__texto">Copyright &copy; 2010-'+currentYear+'</h3>';
+
+const rodape = document.getElementById("rodape");
+if (rodape) {
+    rodape.innerHTML = conteudoRodape;
+}
+
+
+
+  
+for (let index = 1; index < 2; index++) {
+    
+    let newDiv0 = document.createElement('div');
+    let nomeId = 'conteiner' + index;
+    newDiv0.setAttribute('id', nomeId);
+
+    let containerPrincipal = document.getElementById('conteiner-principal');
+
+    containerPrincipal.appendChild(newDiv0);
+
+    for (let index = 0; index < dados.length; index++) {
+        let newSection = document.createElement('section');
+        newSection.setAttribute('class','carrossel');
+        newSection.setAttribute('id','carrossel');
+    
+        newDiv0.appendChild(newSection);
+    
+        let newH2 = document.createElement('h2');
+        newH2.setAttribute('class','carrossel__titulo');
+        newH2.textContent = dados[index].tituloCabecalho;
+        newSection.appendChild(newH2);
+    
+        let newDiv1 = document.createElement('div');
+        newDiv1.setAttribute('class','card');
+        newDiv1.setAttribute('id', 'div1');
+        newSection.appendChild(newDiv1);
+    
+        let newDiv2 = document.createElement('div');
+        newDiv2.setAttribute('class','card__imagem-texto');
+        newDiv2.setAttribute('id', 'div2');
+        newDiv1.appendChild(newDiv2);
+
+        const linkVideo = 'https://youtu.be/' + dados[index].codigoVideo;
+    
+        let newA = document.createElement('a');
+        newA.setAttribute('target','_blank');
+        newA.setAttribute('href', linkVideo);
+        newDiv2.appendChild(newA);
+
+        const imagemVideo = 'https://img.youtube.com/vi/' + dados[index].codigoVideo + '/hqdefault.jpg';
+    
+        let newImg = document.createElement('img');
+        newImg.setAttribute('src', imagemVideo);
+        newImg.setAttribute('alt','Imagem do vídeo Solfejo 03 Software');
+        newImg.setAttribute('class','descricao__imagem');
+        newImg.setAttribute('height','160px');
+        newA.appendChild(newImg);
+    
+        let newP = document.createElement('p');
+        newP.setAttribute('class','texto-card__imagem-texto');
+        newP.textContent = dados[index].resumoVideo;;
+        newDiv2.appendChild(newP);
+    
+        let newDiv3 = document.createElement('div');
+        newDiv3.setAttribute('class','card__botao-solo');
+        newDiv1.appendChild(newDiv3);
+    
+        let newA2 = document.createElement('a');
+        newA2.setAttribute('target','_blank');
+        newA2.setAttribute('href', linkVideo);
+        newA2.setAttribute('class','botao-conhecer');
+        newA2.textContent = 'Assista';
+        newDiv3.appendChild(newA2);
+    
+    }
+    
+}
