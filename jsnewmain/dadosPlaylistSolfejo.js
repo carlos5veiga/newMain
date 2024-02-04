23dfe029c512053bbca6e6ec3e65909d9f698b3c
Carlos Veiga Filho <carlos5veiga@gmail.com>
2024-02-04 08:35:38 -0300

    dados solfejos

--- jsnewmain/dadosPlaylistSolfejo.js
+++ jsnewmain/dadosPlaylistSolfejo.js
@@ -9,7 +9,7 @@ const dados = [
     tituloVideo: 'Solfejo02 - Tônica',
     tituloCabecalho: 'O que é a tônica',
     codigoVideo: '6v4KHwGHXEQ',
-    resumoVideo: 'Perceba auditivamente e entenda o conceito.'
+    resumoVideo: 'Perceba auditivamente e entenda o conceito de tônica, importante elemento musical.'
   },
   {
     tituloVideo: 'Solfejo 03 - Software',
@@ -85,351 +85,339 @@ const dados = [
   },
   {
     tituloVideo: 'Solfejo 15 - Primeiro Solfejo',
-    tituloCabecalho: '',
+    tituloCabecalho: 'Primeiro Solfejo',
     codigoVideo: 'EAOA3NUS0qc',
-    resumoVideo: ''
+    resumoVideo: 'Veja como você já avançou: faça o seu primeiro solfejo.'
   },
   {
     tituloVideo: 'Solfejo 16 - Segundo Solfejo',
-    tituloCabecalho: '',
+    tituloCabecalho: 'Segundo Solfejo',
     codigoVideo: 'My8Jlt57LeY',
-    resumoVideo: ''
+    resumoVideo: 'Ninguém segura você: faça o seu segundo solfejo.'
   },
   {
     tituloVideo: 'Solfejo 17 - Partes de coral',
-    tituloCabecalho: '',
+    tituloCabecalho: 'Partes de coral',
     codigoVideo: 'kM6skY2bJRU',
-    resumoVideo: ''
+    resumoVideo: 'Conhecendo muito material para estudar.'
   },
   {
     tituloVideo: 'Solfejo 18 - Elementos Ritmicos',
-    tituloCabecalho: '',
+    tituloCabecalho: 'Elementos Ritmicos',
     codigoVideo: 'NdfD_V0dk4Y',
-    resumoVideo: ''
+    resumoVideo: 'Vamos mergulhar no aspectos rítmicos, a dor de muita gente.'
   },
   {
     tituloVideo: 'Solfejo 19 - Símbolos rítmicos',
-    tituloCabecalho: '',
+    tituloCabecalho: 'Símbolos rítmicos',
     codigoVideo: 'A28lh01IcZw',
-    resumoVideo: ''
+    resumoVideo: 'Conheça os principais símbolos rítmicos.'
   },
   {
     tituloVideo: 'Solfejo 20 - Compassos',
-    tituloCabecalho: '',
+    tituloCabecalho: 'Compassos',
     codigoVideo: 'VUGYRGlk1AA',
-    resumoVideo: ''
+    resumoVideo: 'Entenda o que é um compasso e tudo vai ficar mais simples.'
   },
   {
     tituloVideo: 'Solfejo 21 - Compassos Binários',
-    tituloCabecalho: '',
+    tituloCabecalho: 'Compassos Binários',
     codigoVideo: 'Oqw3fNlqLDw',
-    resumoVideo: ''
+    resumoVideo: 'Conheça o compasso com 2 tempos.'
   },
   {
     tituloVideo: 'Solfejo 22 - Exemplos binários',
-    tituloCabecalho: '',
+    tituloCabecalho: 'Exemplos binários',
     codigoVideo: 'B27vXvlVdAA',
-    resumoVideo: ''
+    resumoVideo: 'Exemplos de compassos com 2 tempos.'
   },
   {
     tituloVideo: 'Solfejo 23 - Compassos Ternários',
-    tituloCabecalho: '',
+    tituloCabecalho: 'Compassos Ternários',
     codigoVideo: 'zQAj55o7qAU',
-    resumoVideo: ''
+    resumoVideo: 'Conheça o compasso com 3 tempos.'
   },
   {
     tituloVideo: 'Solfejo 24 - Exemplos ternários',
-    tituloCabecalho: '',
+    tituloCabecalho: 'Exemplos ternários',
     codigoVideo: 'mVxQ83tcm1w',
-    resumoVideo: ''
+    resumoVideo: 'Exemplos de compassos com 3 tempos.'
   },
   {
     tituloVideo: 'Solfejo 25 - Compassos quaternários',
-    tituloCabecalho: '',
+    tituloCabecalho: 'Compassos quaternários',
     codigoVideo: 'lC2s9SyGs1A',
-    resumoVideo: ''
+    resumoVideo: 'Conheça o compasso com 4 tempos.'
   },
   {
     tituloVideo: 'Solfejo 26 - Exemplos quaternários',
-    tituloCabecalho: '',
+    tituloCabecalho: 'Exemplos quaternários',
     codigoVideo: 'FAhpkTiYIVM',
-    resumoVideo: ''
+    resumoVideo: 'Exemplos de compassos com 4 tempos.'
   },
   {
     tituloVideo: 'Solfejo 27 - Dobro x Metade',
-    tituloCabecalho: '',
+    tituloCabecalho: 'Dobro x Metade',
     codigoVideo: 'eORjOJvL4co',
-    resumoVideo: ''
+    resumoVideo: 'Importantíssima relação rítmica fácil de perceber.'
   },
   {
     tituloVideo: 'Solfejo 28 - Armaduras com sustenidos',
-    tituloCabecalho: '',
+    tituloCabecalho: 'Armaduras com sustenidos',
     codigoVideo: 'GHzbPJ2euxU',
-    resumoVideo: ''
+    resumoVideo: 'Aprenda a simplificar a escrita musical com as armaduras com sustenidos.'
   },
   {
     tituloVideo: 'Solfejo 29 - Armaduras com bemóis',
-    tituloCabecalho: '',
+    tituloCabecalho: 'Armaduras com bemóis',
     codigoVideo: 'zSpnqICRvKI',
-    resumoVideo: ''
+    resumoVideo: 'Aprenda a simplificar a escrita musical com as armaduras com bemóis.'
   },
   {
     tituloVideo: 'Solfejo 30 - Dois exemplos',
-    tituloCabecalho: '',
+    tituloCabecalho: 'Dois exemplos de armaduras',
     codigoVideo: '9WxJFdF9cws',
-    resumoVideo: ''
+    resumoVideo: 'Veja na prática a importância do conceito de armaduras.'
   },
   {
     tituloVideo: 'Solfejo 31 - Intervalos',
-    tituloCabecalho: '',
+    tituloCabecalho: 'Intervalos',
     codigoVideo: 'pH80v1OzVFA',
-    resumoVideo: ''
+    resumoVideo: 'Quer ganhar mais autonomia? Pratique os intervalos.'
   },
   {
     tituloVideo: 'Solfejo 32 - Segundas Maiores e menores',
-    tituloCabecalho: '',
+    tituloCabecalho: 'Segundas Maiores e menores',
     codigoVideo: '9d-a0jI-IzY',
-    resumoVideo: ''
+    resumoVideo: 'Conheça e perceba os principais intervalos de segundas.'
   },
   {
     tituloVideo: 'Solfejo 33 - Terças maiores e menores',
-    tituloCabecalho: '',
+    tituloCabecalho: 'Terças maiores e menores',
     codigoVideo: 'lEgotD1sKHk',
-    resumoVideo: ''
+    resumoVideo: 'Conheça e perceba os principais intervalos de terças.'
   },
   {
     tituloVideo: 'Solfejo 34 - Oitava justa',
-    tituloCabecalho: '',
+    tituloCabecalho: 'Oitava justa',
     codigoVideo: 'RQv4yL61rLE',
-    resumoVideo: ''
+    resumoVideo: 'Conheça e perceba os principais intervalos de oitavas.'
   },
   {
     tituloVideo: 'Solfejo 35 - Intervalos Justos x Maiores e menores',
-    tituloCabecalho: '',
+    tituloCabecalho: 'Intervalos Justos x Maiores ou menores',
     codigoVideo: 'm4vOmT3qufY',
-    resumoVideo: ''
+    resumoVideo: 'Por que alguns intervalos são classificados como justos enquanto outros são maiores ou menores?'
   },
   {
     tituloVideo: 'Solfejo 36 - Sextas Maiores e menores',
-    tituloCabecalho: '',
+    tituloCabecalho: 'Sextas Maiores e menores',
     codigoVideo: '5yIGHMmLYhc',
-    resumoVideo: ''
+    resumoVideo: 'Conheça e perceba os principais intervalos de sextas.'
   },
   {
     tituloVideo: 'Solfejo 37 - Sétimas Maiores e menores',
-    tituloCabecalho: '',
+    tituloCabecalho: 'Sétimas Maiores e menores',
     codigoVideo: 'mAh_Dt_AZmw',
-    resumoVideo: ''
+    resumoVideo: 'Conheça e perceba os principais intervalos de sétimas.'
   },
   {
     tituloVideo: 'Solfejo 38 - Inversão de intervalos',
-    tituloCabecalho: '',
+    tituloCabecalho: 'Inversão de intervalos',
     codigoVideo: 'd1mtPn229Fo',
-    resumoVideo: ''
+    resumoVideo: 'Conheça esta regrinha para mudar sua vida.'
   },
   {
     tituloVideo: 'Solfejo 39 - Subdivisão binária',
-    tituloCabecalho: '',
+    tituloCabecalho: 'Subdivisão binária',
     codigoVideo: 'Lk0LWJV6HYE',
-    resumoVideo: ''
+    resumoVideo: 'Entenda e perceba a subdivisão por trás dos compassos simples.'
   },
   {
     tituloVideo: 'Solfejo 40 - Clave de fá',
-    tituloCabecalho: '',
+    tituloCabecalho: 'Clave de fá',
     codigoVideo: 'gDjRHvmuz60',
-    resumoVideo: ''
+    resumoVideo: 'Trabalhando com outra clave para ganhar mais autonomia.'
   },
   {
     tituloVideo: 'Solfejo 41 - Exemplos clave de fá',
-    tituloCabecalho: '',
+    tituloCabecalho: 'Exemplos clave de fá',
     codigoVideo: 'hmwXJpFgbyQ',
-    resumoVideo: ''
+    resumoVideo: 'Comece a praticar e veja como é simples.'
   },
   {
     tituloVideo: 'Solfejo 42 - Escala menor primitiva',
-    tituloCabecalho: '',
+    tituloCabecalho: 'Escala menor primitiva',
     codigoVideo: 'Ich5bNj5eEw',
-    resumoVideo: ''
+    resumoVideo: 'Percebendo a escala menor primitava e entendendo a sua relação com a escala maior.'
   },
   {
     tituloVideo: 'Solfejo 43 - Treinando leitura e escrita de música',
-    tituloCabecalho: '',
+    tituloCabecalho: 'Treinando leitura e escrita de música',
     codigoVideo: 'n_WRtA29xZE',
-    resumoVideo: ''
+    resumoVideo: 'Que tal começar a aprender a escrever música?'
   },
   {
     tituloVideo: 'Solfejo 44 - Ditado melódico',
-    tituloCabecalho: '',
+    tituloCabecalho: 'Ditado melódico',
     codigoVideo: 'Svz5AmMOtCY',
-    resumoVideo: ''
+    resumoVideo: 'Um primeiro ditado para você acostumar.'
   },
   {
     tituloVideo: 'Solfejo 45 - Escala menor harmônica',
-    tituloCabecalho: '',
+    tituloCabecalho: 'Escala menor harmônica',
     codigoVideo: 'yMuWqTfWi9I',
-    resumoVideo: ''
+    resumoVideo: 'Uma pequena alteração para grandes diferenças.'
   },
   {
     tituloVideo: 'Solfejo 46 - Escala menor melódica',
-    tituloCabecalho: '',
+    tituloCabecalho: 'Escala menor melódica',
     codigoVideo: 'd3A9jLeDzgw',
-    resumoVideo: ''
+    resumoVideo: 'Subindo a escala com alteração e descendo sem alteração.'
   },
   {
     tituloVideo: 'Solfejo 47 - Escala Bachiana e resumo',
-    tituloCabecalho: '',
+    tituloCabecalho: 'Escala Bachiana e resumo',
     codigoVideo: 'fMD-Q8qal7w',
-    resumoVideo: ''
+    resumoVideo: 'Um resumo com as principais escalas menores.'
   },
   {
     tituloVideo: 'Solfejo 48 - Grupos rítmicos',
-    tituloCabecalho: '',
+    tituloCabecalho: 'Grupos rítmicos',
     codigoVideo: 'u59RMgkflv8',
-    resumoVideo: ''
+    resumoVideo: 'Começando a agrupar símbolos rítmicos.'
   },
   {
     tituloVideo: 'Solfejo 49 - Tercinas ou quiálteras de 3',
-    tituloCabecalho: '',
+    tituloCabecalho: 'Tercinas ou quiálteras de 3',
     codigoVideo: '07VBGhIT7o0',
-    resumoVideo: ''
+    resumoVideo: 'Perceba este importante elemento que será a base dos compassos compostos.'
   },
   {
     tituloVideo: 'Solfejo 50 - Compassos compostos',
-    tituloCabecalho: '',
+    tituloCabecalho: 'Compassos compostos',
     codigoVideo: 'DtaOA3sJOd0',
-    resumoVideo: ''
+    resumoVideo: 'Começando a entender os compassos compostos.'
   },
   {
     tituloVideo: 'Solfejo 51 - Ligaduras de duração',
-    tituloCabecalho: '',
+    tituloCabecalho: 'Ligaduras de duração',
     codigoVideo: 'N8Z7Up1Ye0I',
-    resumoVideo: ''
+    resumoVideo: 'Aprenda a aumentar a duração de um som.'
   },
   {
     tituloVideo: 'Solfejo 52 - Clave de dó',
-    tituloCabecalho: '',
+    tituloCabecalho: 'Clave de dó',
     codigoVideo: 'O3Xvvm1wE50',
-    resumoVideo: ''
+    resumoVideo: 'Uma clave pouco usada mas que você vai entender rapidamente.'
   },
   {
     tituloVideo: 'Solfejo 53 - Relação entre as claves',
-    tituloCabecalho: '',
+    tituloCabecalho: 'Relação entre as claves',
     codigoVideo: 'wR977l8JVcM',
-    resumoVideo: ''
+    resumoVideo: 'Entenda como e porque essas diferentes claves se relacionam.'
   },
   {
     tituloVideo: 'Solfejo 54 - Ligaduras e ponto de aumento',
-    tituloCabecalho: '',
+    tituloCabecalho: 'Ligaduras e ponto de aumento',
     codigoVideo: 'JRwZ7yglHDA',
-    resumoVideo: ''
+    resumoVideo: 'Simplificando a escrita musical com o ponto de aumento.'
   },
   {
     tituloVideo: 'Solfejo 55 - Síncopes',
-    tituloCabecalho: '',
+    tituloCabecalho: 'Síncopes',
     codigoVideo: 'Ofu2-5nyzN0',
-    resumoVideo: ''
+    resumoVideo: 'Conheça este importante elemento musical muito usado na MPB.'
   },
   {
     tituloVideo: 'Anacruse, Tético e Acéfalo',
-    tituloCabecalho: '',
+    tituloCabecalho: 'Anacruse, Tético e Acéfalo',
     codigoVideo: 'aGX17QJauoA',
-    resumoVideo: ''
+    resumoVideo: 'Principais maneiras de classificar uma compasso pelo início da frase.'
   },
   {
     tituloVideo: 'Solfejo 57 - Modo jônio',
-    tituloCabecalho: '',
+    tituloCabecalho: 'Modo jônio',
     codigoVideo: 'VeboQODP20I',
-    resumoVideo: ''
+    resumoVideo: 'Percebendo os modos: Modo jônio. Começando do grau 1.'
   },
   {
     tituloVideo: 'Solfejo 58 - Modo dórico',
-    tituloCabecalho: '',
+    tituloCabecalho: 'Modo dórico',
     codigoVideo: 'EgxyJAESLnY',
-    resumoVideo: ''
+    resumoVideo: 'Percebendo os modos: Modo dórico. Começando do grau 2.'
   },
   {
     tituloVideo: 'Solfejo 59 - Modo frígio',
-    tituloCabecalho: '',
+    tituloCabecalho: 'Modo frígio',
     codigoVideo: '8XaAaph-dfg',
-    resumoVideo: ''
+    resumoVideo: 'Percebendo os modos: Modo frígio. Começando do grau 3.'
   },
   {
     tituloVideo: 'Solfejo 60 - Modo lídio',
-    tituloCabecalho: '',
+    tituloCabecalho: 'Modo lídio',
     codigoVideo: 'V2j4Fv_ql28',
-    resumoVideo: ''
+    resumoVideo: 'Percebendo os modos: Modo lídio. Começando do grau 4.'
   },
   {
     tituloVideo: 'Solfejo 61 - Modo mixolídio',
-    tituloCabecalho: '',
+    tituloCabecalho: 'Modo mixolídio',
     codigoVideo: 'J09UpN_Ti7M',
-    resumoVideo: ''
+    resumoVideo: 'Percebendo os modos: Modo mixolídio. Começando do grau 5.'
   },
   {
     tituloVideo: 'Solfejo 62 - Modo eólio',
-    tituloCabecalho: '',
+    tituloCabecalho: 'Modo eólio',
     codigoVideo: 'pDDoh-uqUgQ',
-    resumoVideo: ''
+    resumoVideo: 'Percebendo os modos: Modo eólio. Começando do grau 6.'
   },
   {
     tituloVideo: 'Solfejo 63 - Modo lócrio',
-    tituloCabecalho: '',
+    tituloCabecalho: 'Modo lócrio',
     codigoVideo: 'RhvCulmPm08',
-    resumoVideo: ''
+    resumoVideo: 'Percebendo os modos: Modo lócrio. Começando do grau 7.'
   },
   {
     tituloVideo: 'Seqüências numéricas - Tutoriais em música',
-    tituloCabecalho: '',
+    tituloCabecalho: 'Seqüências numéricas',
     codigoVideo: '9L4fI4pCO0Q',
-    resumoVideo: ''
-  },
-  {
-    tituloVideo: 'Tutoriais em música - App graus da escala maior',
-    tituloCabecalho: '',
-    codigoVideo: 'jJizNXItjrE',
-    resumoVideo: ''
-  },
-  {
-    tituloVideo: 'Tutoriais em música - Software graus da escala maior',
-    tituloCabecalho: '',
-    codigoVideo: 'S7CWMdhHKV4',
-    resumoVideo: ''
+    resumoVideo: 'Muitas sequências numéricas.'
   },
   {
     tituloVideo: 'Tutoriais em música - Sequencias Numéricas 2',
-    tituloCabecalho: '',
+    tituloCabecalho: 'Sequencias Numéricas 2',
     codigoVideo: 'oo9SdH8y1cw',
-    resumoVideo: ''
+    resumoVideo: 'Mais sequências numéricas para você treinar.'
   },
   {
     tituloVideo: 'Solfejo 68 - Ditados Sequencias Numéricas',
-    tituloCabecalho: '',
+    tituloCabecalho: 'Ditados Sequencias Numéricas',
     codigoVideo: 'k0uJvbMT9JY',
-    resumoVideo: ''
+    resumoVideo: 'Escute, perceba e escreva algumas sequências numéricas.'
   },
   {
     tituloVideo: 'Tutoriais em música - Sequências na pauta',
-    tituloCabecalho: '',
+    tituloCabecalho: 'Sequências na pauta',
     codigoVideo: 'fLHa8M3W7rk',
-    resumoVideo: ''
+    resumoVideo: 'Ótima maneira de praticar tudo isso que você aprendeu aqui.'
   },
   {
     tituloVideo: 'Tutoriais em música - Sequencias Numéricas 3',
-    tituloCabecalho: '',
+    tituloCabecalho: 'Sequencias Numéricas 3',
     codigoVideo: 'vD3d1qNHcBM',
-    resumoVideo: ''
+    resumoVideo: 'Coloque em prática tudo que você vem estudando.'
   },
   {
     tituloVideo: 'Tutoriais em música - Sequencias Numéricas 4',
-    tituloCabecalho: '',
+    tituloCabecalho: 'Sequencias Numéricas 4',
     codigoVideo: 'nNGRfsB1d1k',
-    resumoVideo: ''
+    resumoVideo: 'Não pare de ouvir nem pare de pensar.'
   },
   {
     tituloVideo: 'Solfejo 72 - Melodia simples',
-    tituloCabecalho: '',
+    tituloCabecalho: 'Melodia simples',
     codigoVideo: 'rDwm273u614',
-    resumoVideo: ''
+    resumoVideo: 'Uma super evolução desde que você começou. Concorda comigo?'
   }
 ]
 
