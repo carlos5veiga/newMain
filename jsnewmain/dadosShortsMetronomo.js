c63dfd693b9cfc056d3f49bbcb75d62f289d47cd
carlos5veiga <carlos5veiga@gmail.com>
2025-01-23 17:51:55 -0300

    dados short ritmo

--- jsnewmain/dadosShortsMetronomo.js
+++ jsnewmain/dadosShortsMetronomo.js
@@ -20,6 +20,116 @@ const dados = [
     bpm90: 'tmYAJG7GbWs',
     bpm100: '5ZAUDjrsii4',
     bpm110: 'P--dAgqb9lw'
+  },
+  {
+    tituloVideo: 'Metrônomo 03',
+    tituloCabecalho: 'Metrônomo 3',
+    codigoVideo: 'vlrX1MQvLKs',
+    resumoVideo: 'Neste vídeo eu mostro a situação que realmente necessita uma ligadura de duração. ',
+    bpm70: 'fZfIvM89dbQ',
+    bpm80: 'meCgDfhpKJI',
+    bpm90: 'Pai8Bn3oHSs',
+    bpm100: 'xca-U2pudhQ',
+    bpm110: 'QxgP0ps0csM'
+  },
+  {
+    tituloVideo: 'Metrônomo 04',
+    tituloCabecalho: 'Metrônomo 4',
+    codigoVideo: 'P-aSMWLcwzs',
+    resumoVideo: 'Desenvolva a sua percepção rítmica e aprenda este importante conceito da música.',
+    bpm70: 'a8fpnobWDws',
+    bpm80: 'Z7bB8hjsWxQ',
+    bpm90: 'Rl_bf4eRuQo',
+    bpm100: '5waPKodC16o',
+    bpm110: 'BQL71qAwMNI'
+  },
+  {
+    tituloVideo: 'Metrônomo 05',
+    tituloCabecalho: 'Metrônomo 5',
+    codigoVideo: 'XrKg4RT_rqo',
+    resumoVideo: 'Pratique a leitura rítmica da semibreve e baixe a versão do e-book.',
+    bpm70: 'Gfpl4WQIQTw',
+    bpm80: 'm9VV_fWgMsw',
+    bpm90: 'sQPGkDja8iY',
+    bpm100: 'GVlUxFnsO9Q',
+    bpm110: '542aDKeNF8A'
+  },
+  {
+    tituloVideo: 'Metrônomo 06',
+    tituloCabecalho: 'Metrônomo 6',
+    codigoVideo: 'FppkGXiRftU',
+    resumoVideo: 'Desenvolva a leitura rítmica com este exercício e este super e-book.',
+    bpm70: 't8gt1jjAXHc',
+    bpm80: 'Apf92_5PLNQ',
+    bpm90: '44M0b4-y2OE',
+    bpm100: 'FDQ1FrLL0A8',
+    bpm110: 'V-BDGWaPiwg'
+  },
+  {
+    tituloVideo: 'Metrônomo 07',
+    tituloCabecalho: 'Metrônomo 7',
+    codigoVideo: '9o1MyOJQu-g',
+    resumoVideo: 'Conheça a sutil diferença na escrita dessas pausas e a enorme diferença na execução delas.',
+    bpm70: 'v0DE4-8nCEY',
+    bpm80: 'k7d0MwsWGkg',
+    bpm90: 'wefBiQiXyIo',
+    bpm100: 'EGnsdg32ahg',
+    bpm110: 'HDvHhUBcni0'
+  },
+  {
+    tituloVideo: 'Metrônomo 08',
+    tituloCabecalho: 'Metrônomo 8',
+    codigoVideo: 'MXGYYOrIhxg',
+    resumoVideo: 'Pratique o exercício com mínima pontuada e ligadura de duração e conheça a melhor maneira de estudar musica gratuitamente.',
+    bpm70: 'AnWkAAydT7A',
+    bpm80: 'jBhOBkLTOuw',
+    bpm90: 'B3UcbTAWOG8',
+    bpm100: 'cw9Ltsrk6-g',
+    bpm110: 'b7jOB9M8Fms'
+  },
+  {
+    tituloVideo: 'Metrônomo 09',
+    tituloCabecalho: 'Metrônomo 9',
+    codigoVideo: 'vNkR2xkW2LQ',
+    resumoVideo: 'Esta série é para trazer autonomia para o seu estudo na música.',
+    bpm70: 'MLzf5Ws2ZVc',
+    bpm80: 'Xo5SYhh-Cy4',
+    bpm90: '-6WtdeK8adg',
+    bpm100: 'Lz2KASVkcgI',
+    bpm110: 'CVBefQTUeHY'
+  },
+  {
+    tituloVideo: 'Metrônomo 10',
+    tituloCabecalho: 'Metrônomo 10',
+    codigoVideo: '_QkYm8zzN_I',
+    resumoVideo: 'Com este exercício fantástico lançamos uma nova versão do e-book.',
+    bpm70: 'M5QSl2bd2v4',
+    bpm80: 'cXanm63Qg_4',
+    bpm90: 'PxWU6LtNqTY',
+    bpm100: 'cX3diHCQD9E',
+    bpm110: 'J4Urc5oi6yQ'
+  },
+  {
+    tituloVideo: 'Metrônomo 11',
+    tituloCabecalho: 'Metrônomo 11',
+    codigoVideo: '9mHSxYA4YC0',
+    resumoVideo: 'A partir desse exercício o desafio é maior. Encare as colcheias.',
+    bpm70: 'X79YUDvql-o',
+    bpm80: 'NIYpsptZIUg',
+    bpm90: 'dk4tqSTpUKo',
+    bpm100: 'rCY2a7xLVlY',
+    bpm110: '6VqM-vo4j10'
+  },
+  {
+    tituloVideo: 'Metrônomo 12',
+    tituloCabecalho: 'Metrônomo 12',
+    codigoVideo: 'NsNttAnRz_g',
+    resumoVideo: 'Mergulhando mais fundo no ritmo, vamos trabalhar as pausas de colcheia nos próximos exercícios.',
+    bpm70: 'ph8sAueSoiU',
+    bpm80: 'VDXulC6xhbU',
+    bpm90: 'GGUWE9tfCzo',
+    bpm100: 'mSV4Guuhfqw',
+    bpm110: 'dnBJ_pFPHtM'
   }
 ]
 
