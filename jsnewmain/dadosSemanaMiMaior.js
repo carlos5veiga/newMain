62b25dda0d63ca3f07ae28c564cb6becd55aa68a
carlos5veiga <carlos5veiga@gmail.com>
2026-01-10 22:40:42 -0300

    semana mi maior

---
+++ jsnewmain/dadosSemanaMiMaior.js
@@ -0,0 +1,100 @@
+const dados = [
+  {
+    tituloVideo: 'Mi Maior',
+    tituloCabecalho: 'Mi Maior 01',
+    codigoVideo: 'mWAzHYehl8s',
+    resumoVideo: 'Entenda a tonalidade de Mi Maior.'
+  },
+  {
+    tituloVideo: 'Compasso simples ou composto?',
+    tituloCabecalho: 'Mi Maior 02',
+    codigoVideo: '74rk1cTRJfs',
+    resumoVideo: 'Solfejando em Mi Maior entenda a diferença entre compassos simples e compostos.'
+  },
+  {
+    tituloVideo: 'Tético, Acéfalo e Anacruse',
+    tituloCabecalho: 'Mi Maior 03',
+    codigoVideo: 'JBxGvcr4sZ8',
+    resumoVideo: 'Entenda esses pontos da teoria solfejando em Mi Maior.'
+  },
+  {
+    tituloVideo: 'Compasso 9/8',
+    tituloCabecalho: 'Mi Maior 04',
+    codigoVideo: '_nXStZcxNhE',
+    resumoVideo: 'Pratique o compasso ternário composto solfejando em Mi Maior.'
+  },
+  {
+    tituloVideo: 'Short',
+    tituloCabecalho: '1, 2, 3 - Mi Maior',
+    codigoVideo: 'nX280W2sEKw',
+    resumoVideo: 'Pratique as sequências numéricas em Mi Maior.'
+  },
+  {
+    tituloVideo: 'Short',
+    tituloCabecalho: '1, 2, 3, 4 - Mi Maior',
+    codigoVideo: '9QdRqn1ZGpI',
+    resumoVideo: 'Pratique as sequências numéricas em Mi Maior.'
+  },
+  {
+    tituloVideo: 'Short',
+    tituloCabecalho: '1 - 5: Mi Maior',
+    codigoVideo: 'iTQf21Adnng',
+    resumoVideo: 'Pratique as sequências numéricas em Mi Maior.'
+  },
+  {
+    tituloVideo: 'Short',
+    tituloCabecalho: '1 - 6: Mi Maior',
+    codigoVideo: 'nVVLRXXmBwo',
+    resumoVideo: 'Pratique as sequências numéricas em Mi Maior.'
+  },
+  {
+    tituloVideo: 'Short',
+    tituloCabecalho: '1 - 7: Mi Maior',
+    codigoVideo: 'fEX3k4UBRs0',
+    resumoVideo: 'Pratique as sequências numéricas em Mi Maior.'
+  },
+  {
+    tituloVideo: 'Short',
+    tituloCabecalho: '1 - 8: Mi Maior',
+    codigoVideo: 'fYZa32gufFw',
+    resumoVideo: 'Pratique as sequências numéricas em Mi Maior.'
+  },
+  {
+    tituloVideo: 'Short',
+    tituloCabecalho: '1 - 3 com saltos: Mi Maior',
+    codigoVideo: 'MYBPzD5mMVA',
+    resumoVideo: 'Pratique as sequências numéricas em Mi Maior.'
+  },
+  {
+    tituloVideo: 'Short',
+    tituloCabecalho: '1 - 4 com saltos: Mi Maior',
+    codigoVideo: 'aCeAWOOv2CE',
+    resumoVideo: 'Pratique as sequências numéricas em Mi Maior.'
+  },
+  {
+    tituloVideo: 'Short',
+    tituloCabecalho: '1 - 5 com saltos: Mi Maior',
+    codigoVideo: 'r6fvXkNNQ68',
+    resumoVideo: 'Pratique as sequências numéricas em Mi Maior.'
+  },
+  {
+    tituloVideo: 'Short',
+    tituloCabecalho: '1 - 6 com saltos: Mi Maior',
+    codigoVideo: 'oZwO5HLSMh8',
+    resumoVideo: 'Pratique as sequências numéricas em Mi Maior.'
+  },
+  {
+    tituloVideo: 'Short',
+    tituloCabecalho: '1 - 7 com saltos: Mi Maior',
+    codigoVideo: 'Uh0PGntDhCI',
+    resumoVideo: 'Pratique as sequências numéricas em Mi Maior.'
+  },
+  {
+    tituloVideo: 'Short',
+    tituloCabecalho: '1 - 8 com saltos: Mi Maior',
+    codigoVideo: 'KJPHKBE5eHc',
+    resumoVideo: 'Pratique as sequências numéricas em Mi Maior.'
+  }
+]
+
+export { dados };
